3c62ed5b2349e9c68ee225e09a1513cc1f7dc6a6
Fideline <fideline_bzn@yahoo.com>
2023-05-22 16:19:42 +0200

    setup

---
+++ src/components/MusicBanner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex absolute top-0 w-full bg-black left-0 items-center">
+        <div class=" bg-red-400 p-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
+            </svg>
+        </div>
+        <div class="ml-5 text-white">
+            <p>We wish you a merry Christmas We wish you a merry Christmas We wish you a merry Christmas and a happy new year
+                Good tidings we bring to you and your kin We wish you a merry Christm...</p>
+        </div>
+    </div>
+</template>
+<script src="">
+export default {
+    name: 'MusicPlayer',
+    }
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,24 @@
 <template>
   <div class="home pt-32">
-   <!--<img alt="Vue logo" src="../assets/logo.png">-->
    <p class="mb-4">Compte à rebourd jusqu'à Noel</p>
     <CountDown></CountDown>
+    <TitleEffect></TitleEffect>
+   <!--  <img alt="Vue logo" src="../assets/snowyBall.png">-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CountDown from '@/components/CountDown.vue'
+import TitleEffect from '@/components/TitleEffect.vue';
+//import MusicBanner from '@/components/MusicBanner.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    CountDown
-  }
+    CountDown,
+    TitleEffect,
+    //MusicBanner
+}
 }
 </script>
